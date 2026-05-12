8ed505fd73b220105fe676f9c1c0419f7842665a
xiamu <myjyixi@163.com>
2026-05-12 18:34:02 +0800

    chore: 🔧 update config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,8 +3,14 @@ if status is-interactive
     if type -q starship
         starship init fish | source
     end
+    if type -q fzf
+        fzf --fish | source
+    end
     if type -q zoxide
         zoxide init fish | source
+    end
+    if type -q atuin
+        atuin init fish | source
     end
 
     set -gx EDITOR nvim
